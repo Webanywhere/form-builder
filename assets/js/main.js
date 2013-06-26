2cd5b8fabc9dacf9ae00412abf65e54cf79f03dd
Doug <dougamos@gmail.com>
2013-06-26 17:00:30 +0100

    Starting to allow editing

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,25 +34,19 @@ var main = function($) {
 					nControl++;
 					switch (item.attr('data-control')) {
 						case 'input':
-							newItem = '<div class="col-'+width+' col">';
+							newItem = '<div class="col col-'+width+'">';
 							newItem += '<label for="control_'+nControl+'">Label '+nControl+'</label>';
-							newItem += '<input type="text" id="control_'+nControl+'" placeholder="">';
+							newItem += '<input type="text" id="control_'+nControl+'" placeholder="" name="control_'+nControl+'">';
 							newItem += '</div>';
 							break;
 						case 'textarea':
-							newItem = '<div class="col-'+width+' col">';
+							newItem = '<div class="col col-'+width+'">';
 							newItem += '<label for="control_'+nControl+'">Label '+nControl+'</label>';
-							newItem += '<textarea id="control_'+nControl+'" placeholder="" rows="3"></textarea>';
-							newItem += '</div>';
-							break;
-						case 'textarea':
-							newItem = '<div class="col-'+width+' col">';
-							newItem += '<label for="control_'+nControl+'">Label '+nControl+'</label>';
-							newItem += '<textarea id="control_'+nControl+'" placeholder="" rows="3"></textarea>';
+							newItem += '<textarea id="control_'+nControl+'" placeholder="" rows="3" name="control_'+nControl+'"></textarea>';
 							newItem += '</div>';
 							break;
 						case 'select':
-							newItem = '<div class="col-'+width+' col">';
+							newItem = '<div class="col col-'+width+'">';
 							newItem += '<label for="control_'+nControl+'">Label '+nControl+'</label>';
 							newItem += '<select id="control_'+nControl+'">';
 							newItem += '<option value="1">Option 1</option>';
@@ -61,7 +55,7 @@ var main = function($) {
 							newItem += '</div>';
 							break;
 						case 'selectmultiple':
-							newItem = '<div class="col-'+width+' col">';
+							newItem = '<div class="col col-'+width+'">';
 							newItem += '<label for="control_'+nControl+'">Label '+nControl+'</label>';
 							newItem += '<select id="control_'+nControl+'" multiple="multiple">';
 							newItem += '<option value="1">Option 1</option>';
@@ -70,7 +64,7 @@ var main = function($) {
 							newItem += '</div>';
 							break;
 						case 'checklist':
-							newItem = '<div class="col-'+width+' col">';
+							newItem = '<div class="col col-'+width+'">';
 							newItem += '<div class="checkbox">';
 							newItem += '<label>';
 							newItem += '<input type="checkbox" value="">';
@@ -86,7 +80,7 @@ var main = function($) {
 							newItem += '</div>';
 							break;
 						case 'radiolist':
-							newItem = '<div class="col-'+width+' col">';
+							newItem = '<div class="col col-'+width+'">';
 							newItem += '<div class="radio">';
 							newItem += '<label>';
 							newItem += '<input type="radio" value="">';
@@ -141,19 +135,79 @@ var main = function($) {
 			$('.col, .row').removeClass('selected');
 			$(this).addClass('selected');
 			$('.delete-item').attr('disabled', false);
+			updateProperties();
 			event.stopPropagation();
 		});
 
 		$('.main-panel').on('click', function(event) {
 			$('.col, .row').removeClass('selected');
 			$('.delete-item').attr('disabled', true);
+			updateProperties();
 		})
 	}
 
 	var setDeleteHandler = function() {
 		$('.delete-item').click(function(event) {
 			$('.selected').remove();
+			updateProperties();
 		});
+	}
+
+	var updateProperties = function() {
+		var selected = $('.col.selected');
+		if (selected.length) {
+			selected = $(selected[0]);
+
+			// Clear previous properties
+			$('.properties').html('');
+
+			// Add Id handler
+			var id = selected.find('label').attr('for');
+			$('.properties').append('<label>Id</label><input type="text" value="'+id+'" id="properties-id">');
+			$('#properties-id').bind('input propertychange', function(event) {
+				var value = $(this).val();
+				selected.find('label').attr('for', value);
+				selected.find('input[type=text]').attr('id', value);
+				selected.find('input[type=text]').attr('name', value);
+				selected.find('textarea').attr('id', value);
+				selected.find('textarea').attr('name', value);
+				
+			});
+
+			// Add Width handler
+			var classes = selected.attr('class').split(/\s+/);
+			var columnClass = _.find(classes, function(item) {
+				return item.match(/^col\-[0-9]+$/);
+			});
+			var width = parseInt(columnClass.match(/^col\-([0-9]+)$/)[1]);
+			$('.properties').append('<label>Width (1-12)</label><input type="text" value="'+width+'" id="properties-width">');
+			$('#properties-width').bind('input propertychange', function(event) {
+				var value = parseInt($(this).val());
+				if (value >= 1 && value <= 12) {
+					_.each(_.range(1,13), function(element) {
+						selected.removeClass('col-'+element);
+					})
+					console.log(width);
+					selected.addClass('col-'+value);
+				}
+				
+			});
+
+			// Add Label handler
+			var label = selected.find('label').text();
+			$('.properties').append('<label>Label</label><input type="text" value="'+label+'" id="properties-label">');
+			$('#properties-label').bind('input propertychange', function(event) {
+				var value = $(this).val();
+				selected.find('label').text(value);
+			});
+
+			
+
+
+			$('.properties, .properties-title').show();
+		} else {
+			$('.properties, .properties-title').hide();
+		}
 	}
 
 	return {
