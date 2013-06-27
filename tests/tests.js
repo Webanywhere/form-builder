09238f49efb65975d348dc9782077a061cce46fc
Alice Kaerast <alice@kaerast.info>
2013-06-27 22:32:04 +0100

    improves readability of tests and adds commented out broken test for selecting and deleting rows

--- tests/tests.js
+++ tests/tests.js
@@ -7,7 +7,7 @@ test( "container exists", function() {
 
 // test that there are now rows to begin with
 test( "no rows", function() {
-	ok(typeof($('.row')[0]) == 'undefined', "No rows on-load as expected");
+	equal(typeof($('.row')[0]), 'undefined', "No rows on-load as expected");
 });
 
 // test that we can add rows by clicking the add-row button
@@ -15,9 +15,9 @@ test( "add row", function() {
 	var $addbtn = $( '.add-row');
 	$addbtn.trigger( "click" ); // click the add-row button
 	ok(typeof($('.row')[0]) != 'undefined', "We've added something like a row, it may not be exactly a row");
-	ok($('.row').length == 1, "We have exactly one row-like item");
+	equal($('.row').length, 1, "We have exactly one row-like item");
 	$addbtn.trigger( "click" ); // click the add-row button
-	ok($('.row').length == 2, "Clicking add-row twice gives us exactly two row-like items");
+	equal($('.row').length, 2, "Clicking add-row twice gives us exactly two row-like items");
 });
 
 // test that we can turn guides on and off
@@ -36,3 +36,12 @@ test( 'default width', function() {
 	$('#default-width').slider('value', 4)
 	equal(main.getDefaultWidth(), 4, "Change to 4");
 });
+
+//test( 'select rows', function() {
+//	// @TODO merge with add-row and delete-row tests so we can make this atomic
+//	equal(typeof($( '.row .selected')[0]), 'undefined', "Nothing selected to begin with");
+//	$( '.add-row' ).trigger( 'click' );
+//	$( '.row').trigger( 'click' );
+//	equal($( '.row .selected').length, 1, "Added and selected a row");
+//	$( '.delete-item').trigger( 'click' );
+//});
