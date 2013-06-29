6a412c970101d93804fd0c0ae58b52e184b950fa
Alice Kaerast <alice@kaerast.info>
2013-06-29 10:10:56 +0100

    moves add/select/delete rows tests into a single atomic unit test

--- tests/tests.js
+++ tests/tests.js
@@ -5,19 +5,24 @@ test( "container exists", function() {
 	ok( $panel, "Panel exists on-load as expected");
 });
 
-// test that there are now rows to begin with
-test( "no rows", function() {
-	equal(typeof($('.row')[0]), 'undefined', "No rows on-load as expected");
-});
-
 // test that we can add rows by clicking the add-row button
-test( "add row", function() {
+test( "add remove rows", function() {
+	equal(typeof($('.row')[0]), 'undefined', "No rows on-load as expected");
 	var $addbtn = $( '.add-row');
 	$addbtn.trigger( "click" ); // click the add-row button
 	ok(typeof($('.row')[0]) != 'undefined', "We've added something like a row, it may not be exactly a row");
 	equal($('.row').length, 1, "We have exactly one row-like item");
 	$addbtn.trigger( "click" ); // click the add-row button
 	equal($('.row').length, 2, "Clicking add-row twice gives us exactly two row-like items");
+	$('.row').trigger( 'click' ); // select a row
+	equal($('.selected').length, 1, "Selected a row");
+	$('.delete-item').trigger( 'click' ); // click the delete button
+	equal($('.row').length, 1, "Delete a single row");
+	$('.row').trigger( 'click' );  // select a row
+	$('.delete-item').trigger( 'click' ); // click the delete button
+	equal($('.row').length, 0, "Delete last row");
+	$('.main-panel').trigger( 'click' );  // make sure nothing's selected
+	equal($('.delete-item').prop('disabled'), true, "Ensure delete button is disabled when nothing selected");
 });
 
 // test that we can turn guides on and off
@@ -36,12 +41,3 @@ test( 'default width', function() {
 	$('#default-width').slider('value', 4)
 	equal(main.getDefaultWidth(), 4, "Change to 4");
 });
-
-//test( 'select rows', function() {
-//	// @TODO merge with add-row and delete-row tests so we can make this atomic
-//	equal(typeof($( '.row .selected')[0]), 'undefined', "Nothing selected to begin with");
-//	$( '.add-row' ).trigger( 'click' );
-//	$( '.row').trigger( 'click' );
-//	equal($( '.row .selected').length, 1, "Added and selected a row");
-//	$( '.delete-item').trigger( 'click' );
-//});
