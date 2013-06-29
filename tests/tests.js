5a2cbfda32ad29ef37778bd0ecb98ef8d90e740c
Alice Kaerast <alice@kaerast.info>
2013-06-29 13:40:05 +0100

    makes tests dom more similar to live dom

--- tests/tests.js
+++ tests/tests.js
@@ -7,20 +7,19 @@ test( "container exists", function() {
 
 // test that we can add rows by clicking the add-row button
 test( "add remove rows", function() {
-	equal(typeof($('.row')[0]), 'undefined', "No rows on-load as expected");
+	equal(typeof($('.ui-sortable')[0]), 'undefined', "No rows on-load as expected");
 	var $addbtn = $( '.add-row');
 	$addbtn.trigger( "click" ); // click the add-row button
-	ok(typeof($('.row')[0]) != 'undefined', "We've added something like a row, it may not be exactly a row");
-	equal($('.row').length, 1, "We have exactly one row-like item");
+	equal($('.ui-sortable').length, 1, "We have exactly one row-like item");
 	$addbtn.trigger( "click" ); // click the add-row button
-	equal($('.row').length, 2, "Clicking add-row twice gives us exactly two row-like items");
+	equal($('.ui-sortable').length, 2, "Clicking add-row twice gives us exactly two row-like items");
 	$('.row').trigger( 'click' ); // select a row
 	equal($('.selected').length, 1, "Selected a row");
 	$('.delete-item').trigger( 'click' ); // click the delete button
-	equal($('.row').length, 1, "Delete a single row");
+	equal($('.ui-sortable').length, 1, "Delete a single row");
 	$('.row').trigger( 'click' );  // select a row
 	$('.delete-item').trigger( 'click' ); // click the delete button
-	equal($('.row').length, 0, "Delete last row");
+	equal($('.ui-sortable').length, 0, "Delete last row");
 	$('.main-panel').trigger( 'click' );  // make sure nothing's selected
 	equal($('.delete-item').prop('disabled'), true, "Ensure delete button is disabled when nothing selected");
 });
@@ -40,4 +39,10 @@ test( 'default width', function() {
 	equal(main.getDefaultWidth(), 12, "Default to 12");
 	$('#default-width').slider('value', 4)
 	equal(main.getDefaultWidth(), 4, "Change to 4");
+});
+
+// test that we can drag and drop items and then remove them
+test( 'drag and drop', function() {
+	//pending
+	expect(0);
 });
